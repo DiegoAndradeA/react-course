287795f88683d62b235d38bc652d9000f94ca3ac
Diego Andrade <dandradea1@gmail.com>
2024-12-03 23:31:03 -0300

    pepo6

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,26 +1,13 @@
-import TaskList from "./TaskList"
-import TaskForm from "./TaskForm"
-import { tasks as data } from "./tasks";
-import {useState, useEffect} from "react"
+import TaskList from "./components/TaskList"
+import TaskForm from "./components/TaskForm"
+ 
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-
-  useEffect(()=> {
-    setTasks(data)
-  }, [])
-
-  function createTask(taskTitle) {
-    setTasks([...tasks, {
-      title: taskTitle,
-      id: tasks.length,
-      description: "nueva tarea"
-    }])
-  }
+  
 
   return <div> 
-    <TaskForm createTask={createTask}/>
-    <TaskList tasks={tasks}/>
+    <TaskForm />
+    <TaskList/>
   </div>
 }
 
---
+++ vite-project/src/components/TaskForm.jsx
@@ -0,0 +1,40 @@
+import { useState, useContext } from "react"
+import {TaskContext} from "../context/TaskContext"
+
+function TaskForm() {
+
+    const [title, setTitle] = useState("")
+    const [description, setDescription] = useState("")
+    const {createTask} = useContext(TaskContext)
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        createTask({
+            title,
+            description
+        });
+        setDescription("")
+        setTitle("")
+    };  
+
+  return (
+
+        <form onSubmit={handleSubmit}>
+            <input placeholder="Escribe tu tarea"
+                onChange={(e) => setTitle(e.target.value)}
+                value = {title}
+                autoFocus
+            />
+            <textarea placeholder="Escribe la descripcion"
+                onChange={(e) => setDescription(e.target.value)}
+                value = {description}
+                />
+            
+            <button>
+                Guardar
+            </button>
+        </form>
+  )
+}
+
+export default TaskForm
