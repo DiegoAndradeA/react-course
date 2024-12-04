4ec1663d586ebb557504abdf3048eb49f83b974e
Diego Andrade <dandradea1@gmail.com>
2024-12-03 22:01:57 -0300

    pepo5

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,5 +1,27 @@
+import TaskList from "./TaskList"
+import TaskForm from "./TaskForm"
+import { tasks as data } from "./tasks";
+import {useState, useEffect} from "react"
+
 function App() {
-  return <h1>Hola feo</h1>
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(()=> {
+    setTasks(data)
+  }, [])
+
+  function createTask(taskTitle) {
+    setTasks([...tasks, {
+      title: taskTitle,
+      id: tasks.length,
+      description: "nueva tarea"
+    }])
+  }
+
+  return <div> 
+    <TaskForm createTask={createTask}/>
+    <TaskList tasks={tasks}/>
+  </div>
 }
 
 export default App
---
+++ vite-project/src/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "mi primer tarea",
+    description: "descripcion de mi primer tarea",
+  },
+  {
+    id: 1,
+    title: "mi pepo tarea",
+    description: "descripcion de mi primer tarea",
+  },
+  {
+    id: 2,
+    title: "mi pingu tarea",
+    description: "descripcion de mi primer tarea",
+  }
+];
